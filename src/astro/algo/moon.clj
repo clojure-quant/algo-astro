dbde715d6b21b25be77c5ba9eedff9250163b9a8
awb99 <awb99@gmail.com>
2024-03-20 22:07:31 -0500

    logging

--- src/astro/algo/moon.clj
+++ src/astro/algo/moon.clj
@@ -1,20 +1,22 @@
 (ns astro.algo.moon
-  (:require 
+  (:require
    [tablecloth.api :as tc]
    [indicator :refer [prior]]
-    [astro.moon :refer [moon-phase-from-instant phase->text]])
-  )
+   [astro.moon :refer [moon-phase-from-instant phase->text]]))
 
 ; :text "🌑"
 
 
 (defn moon-algo [env opts bar-ds]
   (let [date (:date bar-ds)
+        _ (println "MOON PHASE CALC..")
         phase (map moon-phase-from-instant date)
+        _ (println "CHANGE CALC..")
         phase-prior (prior phase)
         change (map (fn [p p1]
                       (if (= p p1) nil phase))
                     phase phase-prior)]
+    (println "moon-algo finished!")
     (tc/add-column bar-ds :moon-phase change)))
 
 
