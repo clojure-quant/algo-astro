7317a7f9df3d9a421ac4ec0fd62973060b5bc1ac
awb99 <awb99@gmail.com>
2024-05-02 16:18:47 -0500

    backtest improved

--- resources/quanta/notebook/study/astro_moon.clj
+++ resources/quanta/notebook/study/astro_moon.clj
@@ -5,32 +5,29 @@
    [ta.interact.template :refer [load-template show-result]]
    [ta.algo.backtest :refer [backtest-algo backtest-algo-date]]
    [ta.trade.backtest.from-position :refer [signal->roundtrips]]
-   [ta.trade.roundtrip.core :refer [metrics]]
-   [ta.viz.ds.metrics :refer [metrics-render-spec-impl]]
-   ))
+   [ta.trade.roundtrip.core :refer [roundtrip-stats]]
+   [ta.viz.trade.core :refer [roundtrip-stats-ui]]))
 
 (def template (load-template :moon-signal))
 
-template
+;template
 
 ;; here we use the default options from the template; but
 ;; we could change them if we wanted to.
 
-(def algo-spec (:algo template))
-
-
-algo-spec
+(def algo-spec (-> (:algo template)
+                   (assoc :trailing-n 5000)))
 
 (def ds @(backtest-algo-date :bardb-dynamic  algo-spec
          (t/zoned-date-time "2024-02-22T17:00-05:00[America/New_York]")))
 
-ds
+;ds
 
 (def roundtrips (signal->roundtrips ds))
 
-roundtrips
+;roundtrips
 
 (-> roundtrips 
-    metrics
-    metrics-render-spec-impl)
+    roundtrip-stats
+    roundtrip-stats-ui)
 
